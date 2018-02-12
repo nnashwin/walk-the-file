92cfe1580c052e5cda64e4cd4de9a9fa04b9aab7
Tyler Boright <lupin3.ken@gmail.com>
2018-02-12 10:26:15 -0800

    adds two new tests to test and make sure that the more shallow level is tested for the file before the deep ones; changes the implementation to use bfs instead of dfs (by changing the concat of the queue to appending the deeper constructed files to the end of the queue rather than at the beginning)

--- test.js
+++ test.js
@@ -1,3 +1,22 @@
 import test from 'ava';
+import findFile from './index';
 
+test('findFile finds the correct file in a dir which has two nested dirs and only one has the file', t => {
+    const expected = "./fixtures/lateLevel/b/index.html"
+    t.is(findFile('./fixtures/lateLevel', 'index.html'), expected);
+});
+ 
+test('findFile finds the correct file even if it is nested deep in the structure', t => {
+    const expected = "./fixtures/nested/dir/furtherNest/index.html"
+    t.is(findFile('./fixtures/nested', 'index.html'), expected);
+});
 
+test('findFile finds and returns only one version of the file when the nested dir occurs before the filename in the alphabet', t => {
+    const expected = "./fixtures/twoLevels/index.html";
+    t.is(findFile('./fixtures/twoLevels', 'index.html'), expected);
+})
+
+test('findFile finds and returns only one version of the file when the nested dir occurs after the filename in the alphabet', t => {
+    const expected = "./fixtures/twoLevelsAfter/index.html";
+    t.is(findFile('./fixtures/twoLevelsAfter', 'index.html'), expected);
+})
