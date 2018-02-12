e2446cf0b6e3a2f6c47bf1bafd62bc06bce309d5
ttymed <tyler.boright@gmail.com>
2018-02-11 22:30:33 -0800

    adds a tests to see if the file exists before checking it if is a directory in order to not error out when the file path is wrong

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ module.exports = (path, fileName) => {
 		const file = queue.shift();
 		if (file === fileName) {
 			return path + "/" + fileName;
-		} else if (fs.statSync(path + "/" + file).isDirectory()) {
+		} else if (fs.existsSync(path + "/" + file) && fs.statSync(path + "/" + file).isDirectory()) {
 			// uses shift and concatenation from fs.readdirSync to keep the in-order dfs in alphabetical order
 			queue = fs.readdirSync(path + "/" + file).concat(queue);
 			path = path + "/" + file;
