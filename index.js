f0e16ec9db59d8153a2e16bbe66e5e10c90be3d0
ttymed <tyler.boright@gmail.com>
2018-02-11 21:22:48 -0800

    removes the map filter structure in order to better implement dfs searching of the dir structure to only find one dir; turns modules into more of a pure module since recursion is not required

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 const fs = require('fs');
 const stats = require('fs').Stats;
 
-const isNotUndefined = val => val !== undefined;
-
-// adds a name to the export in order to call it recursively
-module.exports = function findFilePath(path, fileName) {
-	return fs.readdirSync(path).map((file) => {
+module.exports = (path, fileName) => {
+	let queue = fs.readdirSync(path);
+	let foundPath = path;
+	// shifts off the first .git dir because it is unnecessary
+	queue.shift();
+	// use In-order to return the first instance of the fileName
+	while (queue.length > 0) {
+		const file = queue.shift();
 		if (file === fileName) {
 			return path + "/" + fileName;
-		} 
-
-		if(fs.statSync(path + "/" + file).isDirectory()) {
-			return findFilePath(path + "/" + file, fileName);
+		} else if (fs.statSync(path + "/" + file).isDirectory()) {
+			// uses shift and concatenation from fs.readdirSync to keep the in-order dfs in alphabetical order
+			queue = fs.readdirSync(path + "/" + file).concat(queue);
+			path = path + "/" + file;
 		}
-	})
-	// filters out all undefined
-	.filter(isNotUndefined)
-	// reduces the string into an empty string if found
-	.reduce((res, foundPath) => {
-		return res + foundPath;
-	}, "");
+	}
+	return "";
 }
