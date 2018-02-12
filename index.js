7bab7eee5711d80ce372b6acf5274ce98054dab7
ttymed <tyler.boright@gmail.com>
2018-02-11 22:50:46 -0800

    changes the implementation to map filestrs to the file itself before concatenating; splits those filenames off before comparing with the parameter file name

--- index.js
+++ index.js
@@ -3,18 +3,15 @@ const stats = require('fs').Stats;
 
 module.exports = (path, fileName) => {
 	let queue = fs.readdirSync(path);
-	let foundPath = path;
-	// shifts off the first .git dir because it is unnecessary
-	queue.shift();
 	// use In-order to return the first instance of the fileName
 	while (queue.length > 0) {
 		const file = queue.shift();
-		if (file === fileName) {
-			return path + "/" + fileName;
+		if (file.split("/")[file.split("/").length - 1] === fileName) {
+			return path + "/" + file;
 		} else if (fs.existsSync(path + "/" + file) && fs.statSync(path + "/" + file).isDirectory()) {
 			// uses shift and concatenation from fs.readdirSync to keep the in-order dfs in alphabetical order
-			queue = fs.readdirSync(path + "/" + file).concat(queue);
-			path = path + "/" + file;
+			// maps to append the nested string to the filestring
+			queue = fs.readdirSync(path + "/" + file).map(fileStr => file + "/" + fileStr).concat(queue);
 		}
 	}
 	return "";
