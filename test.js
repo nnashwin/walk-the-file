7dfe14faf06e18bd95287006c61ae2cdb517f14e
ttymed <tyler.boright@gmail.com>
2018-09-30 19:56:04 -0700

    adds test to test the shallow findFile and ensure that it finds the non-nested file first

--- test.js
+++ test.js
@@ -1,5 +1,8 @@
 import test from 'ava';
+const findFile = require('./index');
+const path = require('path');
 
-test('foo', t => {
-	t.pass();
+test('finds shallowly first occurrence of file in nested dirs', t => {
+	const testDir = path.join('fixture', 'testDeep');
+	t.is(findFile(testDir, 'index'), path.join('fixture', 'testDeep', 'index'));
 });
